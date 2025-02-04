310bd3101072f62d5f6c24cbcc87906c74982cb2
Nitika-singh-4 <nitikasingh262@gmail.com>
2025-02-04 09:08:54 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/ping', (req, res)=>{
-    res.json({message: 'pong'});
-});
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>{
-    console.log(`Server is running on the port ${PORT}`);
-});
